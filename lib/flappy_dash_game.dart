4b49fd63b1a538bd2eb4fce9601ee556a5a4f9e9
Jeevan kumar kushwaha <jeevankushwaha461@gmail.com>
2024-12-14 16:33:11 +0545

    lession 2

--- lib/flappy_dash_game.dart
+++ lib/flappy_dash_game.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/palette.dart';
 
 class FlappyDashGame extends FlameGame {
   FlappyDashGame()
@@ -10,34 +11,33 @@ class FlappyDashGame extends FlameGame {
           world: FlappyDashWorld(),
           camera: CameraComponent.withFixedResolution(width: 600, height: 1000),
         );
+}
 
+class FlappyDashWorld extends World {
   @override
-  void onGameResize(Vector2 size) {
-    // TODO: implement onGameResize
-    super.onGameResize(size);
-    print('onGameResize called $size');
-  }
-
-  @override
-  Future<void> onLoad() async {
-    // TODO: implement load
+  void onLoad() {
     super.onLoad();
-    print('onLoad called');
+    add(Dash());
+  }
+}
+
+class Dash extends PositionComponent {
+  Dash()
+      : super(
+          position: Vector2(100, 100),
+          size: Vector2(50, 50),
+        );
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    canvas.drawRect(size.toRect(), BasicPalette.green.paint());
   }
 
   @override
   void update(double dt) {
-    // TODO: implement update
     super.update(dt);
-    print('update called $dt');
-  }
-
-  @override
-  void render(Canvas canvas) {
-    // TODO: implement render
-    super.render(canvas);
-    print('render called $canvas');
+    position = Vector2(0, 100 * dt);
+    angle = angle + 0.01;
   }
 }
-
-class FlappyDashWorld extends World {}
